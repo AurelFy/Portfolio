6f87d1e8282b0669c30d46fa8fd6a99d36ffbfab
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-05-01 23:11:27 +0200

    menu burger

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,20 @@
-console.info('Hello world');
+"use strict";
+
+var menuBtn = document.querySelector(".menu--burger");
+menuBtn.addEventListener("click", openMenu);
+
+function openMenu(){
+  var menu = document.querySelector(".menu--links");
+  menu.classList.toggle("menu--links__hidden");
+  }
+
+var menuLinks = document.querySelectorAll(".menu--links__el");
+
+menuLinks.forEach(function(link) {
+    link.addEventListener("click", hideMenu);
+});
+    
+function hideMenu() {
+    var menu = document.querySelector(".menu--links");
+    menu.classList.toggle("menu--links__hidden");
+} 

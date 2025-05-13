687d5fd89c12b7f0d907935f672dcd889de2b910
Aurélien Feye <aurelien.feye@student.heaj.be>
2025-05-13 12:39:45 +0200

    nav desktop + curseur

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -19,37 +19,9 @@ function hideMenu() {
     menu.classList.toggle("menu--links__hidden");
 } 
 
-const menu = document.querySelector('.menu');
+const cursor = document.querySelector('.cursor--center');
 
-function sup(el1, el2) {
-  const r1 = el1.getBoundingClientRect();
-  const r2 = el2.getBoundingClientRect();
-
-  return !(
-    r1.right < r2.left ||
-    r1.left > r2.right ||
-    r1.bottom < r2.top ||
-    r1.top > r2.bottom
-  );
-}
-
-function check() {
-  const elements = document.querySelectorAll('body *:not(.menu)');
-  let contact = false;
-
-  elements.forEach(el => {
-    if (sup(menu, el)) {
-      contact = true;
-    }
-  });
-
-  if (contact) {
-    menu.classList.add('menu--sup');
-  } else {
-    menu.classList.remove('menu--sup');
-  }
-}
-
-window.addEventListener('scroll', check);
-window.addEventListener('resize', check);
-window.addEventListener('load', check);
+window.addEventListener('mousemove', (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
